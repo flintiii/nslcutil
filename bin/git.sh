65be80ef3eeb92117345981a9c438acb3f451478
Paul Flint <flint@flint.com>
2016-02-10 08:48:20 -0500

    Finally working with example.csv, need command line next

--- bin/git.sh
+++ bin/git.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 # 02/05/2016 03:21:21 PM flint
 # makes commits easy
-git add .
-git commit -m \"$1\"
+git add . && \
+git add -u && \
+git commit -m "$(read -p 'Commit description: ')" && \
+echo "Committing to goddard Redmine:"
 git push nslcutil
-
+echo "Committing to Github:"
+git push github
+echo \"$1\"
+exit
+git push origin HEAD
+git add .
+git commit -m $1
+git push nslcutil
+git push github
