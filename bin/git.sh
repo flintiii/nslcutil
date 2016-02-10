1872288770e8f822834d73cb3e0263847d27a25c
Paul Flint <flint@flint.com>
2016-02-10 11:17:58 -0500

    The csv test module is working.  The next is the tab module.

--- bin/git.sh
+++ bin/git.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 # 02/05/2016 03:21:21 PM flint
 # makes commits easy
+# 02/10/2016 08:52:59 AM added changes suggested in:
+# SOURCE:http://stackoverflow.com/questions/8482843/git-commit-bash-script answer 5
 git add . && \
 git add -u && \
+git commit --all && \
 git commit -m "$(read -p 'Commit description: ')" && \
-echo "Committing to goddard Redmine:"
-git push nslcutil
-echo "Committing to Github:"
-git push github
+echo "Committing to goddard Redmine:"; git push nslcutil
+echo "Committing to Github:";git push gitssl
 echo \"$1\"
 exit
-git push origin HEAD
-git add .
-git commit -m $1
-git push nslcutil
-git push github
